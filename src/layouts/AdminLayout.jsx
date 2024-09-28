6e6b2de72346a360b3ab7ce1c5f0199eab13bd1f
garnet888 <garnet.anar19@gmail.com>
2024-09-28 18:39:03 +0800

    upgrade next and minor update

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,11 +1,18 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { disableSmallMenu, enableSmallMenu, getSmallMenu } from '@/lib/helper';
 import Sidebar from '@/utils/Sidebar';
 
 const AdminLayout = ({ children }) => {
-  const [smallMenu, setSmallMenu] = useState(getSmallMenu() === 'YES');
+  const [isMounted, setIsMounted] = useState(false);
+  const [smallMenu, setSmallMenu] = useState();
+
+  useEffect(() => {
+    setSmallMenu(getSmallMenu() === 'YES');
+
+    setIsMounted(true);
+  }, []);
 
   const smallMenuHandler = () => {
     setSmallMenu((prev) => !prev);
@@ -17,26 +24,27 @@ const AdminLayout = ({ children }) => {
     }
   };
 
-  return (
-    <main className='admin_layout background_img_2 bg-[#f0f2f5]'>
-      <Sidebar
-        smallMenu={smallMenu}
-        smallMenuHandler={smallMenuHandler}
-      />
+  if (isMounted) {
+    return (
+      <main className='admin_layout background_img_2 bg-[#f0f2f5]'>
+        <Sidebar smallMenu={smallMenu} smallMenuHandler={smallMenuHandler} />
 
-      <div
-        style={{ gridArea: 'adn_content' }}
-        className={[
-          smallMenu
-            ? 'pl-[12px] ml-4 sm:ml-0 sm:w-adn_small_content'
-            : 'ml-4 sm:ml-0 sm:w-adn_content',
-          'width_effect h-screen overflow-auto',
-        ]}
-      >
-        {children}
-      </div>
-    </main>
-  );
+        <div
+          style={{ gridArea: 'adn_content' }}
+          className={[
+            smallMenu
+              ? 'pl-[12px] ml-4 sm:ml-0 sm:w-adn_small_content'
+              : 'ml-4 sm:ml-0 sm:w-adn_content',
+            'width_effect h-screen overflow-auto',
+          ]}
+        >
+          {children}
+        </div>
+      </main>
+    );
+  }
+
+  return <main className='admin_layout background_img_2 bg-[#f0f2f5]' />;
 };
 
 export default AdminLayout;
--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,13 +1,13 @@
 const SMALL_MENU_KEY = 'SMALL_MENU';
 
 export const getSmallMenu = () => {
-  return window.localStorage.getItem(SMALL_MENU_KEY);
+  return localStorage.getItem(SMALL_MENU_KEY);
 };
 
 export const enableSmallMenu = () => {
-  window.localStorage.setItem(SMALL_MENU_KEY, 'YES');
+  localStorage.setItem(SMALL_MENU_KEY, 'YES');
 };
 
 export const disableSmallMenu = () => {
-  window.localStorage.removeItem(SMALL_MENU_KEY);
+  localStorage.removeItem(SMALL_MENU_KEY);
 };
