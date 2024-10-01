024fceaeebe1bf26209d18d518d74616409a418f
anar19 <anar@digitalfinex.mn>
2024-10-01 18:29:07 +0800

    not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,25 @@
-import BaseLayout from '@/layouts/BaseLayout';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const title = 'Хуудас олдсонгүй';
-
-export const metadata = { title };
+export const metadata = { title: 'Хуудас олдсонгүй' };
 
 const NotFound = () => {
   return (
-    <BaseLayout>
-      <main className='h-full grid place-content-center'>
-        <h1>{title}...</h1>
-      </main>
-    </BaseLayout>
+    <main className='grid place-content-center text-center sm:text-start px-6 mt-36'>
+      <Link href='/'>
+        <Image
+          className='w-[112px] h-[38px] object-contain mx-auto sm:mx-0'
+          src='/images/logo_name.png'
+          alt='Logo'
+          width={300}
+          height={300}
+          priority
+        />
+      </Link>
+
+      <p className='text-red-500 text-2xl font-medium mt-2'>404!</p>
+      <p className='text-zinc-500 text-xl'>Таны хандсан хуудас байхгүй байна</p>
+    </main>
   );
 };
 
